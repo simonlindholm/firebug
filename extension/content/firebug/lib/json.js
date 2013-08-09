35f5f34289b7b22cb539d71ab203063ac419e1fc
Simon Lindholm <simon.lindholm10@gmail.com>
2013-08-09 02:15:46 +0200

    Improve performance of JSON parsing

--- extension/content/firebug/lib/json.js
+++ extension/content/firebug/lib/json.js
@@ -1,9 +1,11 @@
 /* See license.txt for terms of usage */
+/*global define:1*/
 
 define([
     "firebug/lib/trace"
 ],
 function(FBTrace) {
+"use strict";
 
 // ********************************************************************************************* //
 // Constants
@@ -15,81 +17,76 @@ var Json = {};
 
 Json.parseJSONString = function(jsonString, originURL)
 {
+    var regex, matches;
     if (FBTrace.DBG_JSONVIEWER)
         FBTrace.sysout("jsonviewer.parseJSON; " + jsonString);
 
-    // See if this is a Prototype style *-secure request.
-    var regex = new RegExp(/\s*\/\*-secure-([\s\S]*)\*\/\s*$/);
-    var matches = regex.exec(jsonString);
-
-    if (matches)
+    var first = firstNonWs(jsonString);
+    if (first !== "[" && first !== "{")
     {
-        jsonString = matches[1];
+        // This (probably) isn't pure JSON. Let's try to strip various sorts
+        // of XSSI protection/wrapping and see if that works better.
 
-        if (jsonString[0] == "\\" && jsonString[1] == "n")
-            jsonString = jsonString.substr(2);
-
-        if (jsonString[jsonString.length-2] == "\\" && jsonString[jsonString.length-1] == "n")
-            jsonString = jsonString.substr(0, jsonString.length-2);
-    }
-
-    if (jsonString.indexOf("&&&START&&&"))
-    {
-        regex = new RegExp(/&&&START&&& (.+) &&&END&&&/);
+        // Prototype-style secure requests
+        regex = /^\*\/\*-secure-([\s\S]*)\*\/\s*$/;
         matches = regex.exec(jsonString);
         if (matches)
+        {
             jsonString = matches[1];
+
+            if (jsonString[0] === "\\" && jsonString[1] === "n")
+                jsonString = jsonString.substr(2);
+
+            if (jsonString[jsonString.length-2] === "\\" && jsonString[jsonString.length-1] === "n")
+                jsonString = jsonString.substr(0, jsonString.length-2);
+        }
+
+        // Google-style (?) delimiters
+        if (jsonString.indexOf("&&&START&&&") !== -1)
+        {
+            regex = /&&&START&&&([\s\S]*)&&&END&&&/;
+            matches = regex.exec(jsonString);
+            if (matches)
+                jsonString = matches[1];
+        }
+
+        // while(1);, for(;;);, and )]}'
+        regex = /^\s*(\)\]\}[^\n]*\n|while\(1\);|for\(;;\);)([\s\S]*)/;
+        matches = regex.exec(jsonString);
+        if (matches)
+            jsonString = matches[2];
+
+        // JSONP
+        regex = /^\s*([A-Za-z0-9_$.]+\s*(?:\[.*\]|))\s*\(([\s\S]*)\)/;
+        matches = regex.exec(jsonString);
+        if (matches)
+            jsonString = matches[2];
     }
 
     try
     {
-        var s = Components.utils.Sandbox(originURL);
-
-        // throw on the extra parentheses
-        return Components.utils.evalInSandbox("(" + jsonString + ")", s);
+        return JSON.parse(jsonString);
     }
-    catch(e)
+    catch (exc)
     {
         if (FBTrace.DBG_JSONVIEWER)
-            FBTrace.sysout("jsonviewer.parseJSON FAILS on "+originURL+" for \""+jsonString+
-                "\" with EXCEPTION "+e, e);
-    }
-
-    // Let's try to parse it as JSONP.
-    var reJSONP = /^\s*([A-Za-z0-9_.]+\s*(?:\[.*\]|))\s*\(.*\)/;
-    var m = reJSONP.exec(jsonString);
-    if (!m || !m[1])
-        return null;
-
-    if (FBTrace.DBG_JSONVIEWER)
-        FBTrace.sysout("jsonviewer.parseJSONP; " + jsonString);
-
-    var callbackName = m[1];
-
-    if (FBTrace.DBG_JSONVIEWER)
-        FBTrace.sysout("jsonviewer.parseJSONP; Look like we have a JSONP callback: " + callbackName);
-
-    // Replace the original callback (it can be e.g. foo.bar[1]) with simple function name.
-    jsonString = jsonString.replace(callbackName, "callback");
-
-    try
-    {
-        var s = Components.utils.Sandbox(originURL);
-        s["callback"] = function(object) { return object; };
-        return Components.utils.evalInSandbox(jsonString, s);
-    }
-    catch(ex)
-    {
-        if (FBTrace.DBG_JSONVIEWER)
-            FBTrace.sysout("jsonviewer.parseJSON EXCEPTION", e);
+            FBTrace.sysout("jsonviewer.parseJSON FAILS on "+originURL+" for \"" + jsonString +
+                "\" with EXCEPTION " + exc, exc);
     }
 
     return null;
 };
 
-Json.parseJSONPString = function(jsonString, originURL)
+function firstNonWs(str)
 {
-};
+    for (var i = 0, len = str.length; i < len; i++)
+    {
+        var ch = str[i];
+        if (ch !== " " && ch !== "\n" && ch !== "\t" && ch !== "\r")
+            return ch;
+    }
+    return "";
+}
 
 // ********************************************************************************************* //
 
