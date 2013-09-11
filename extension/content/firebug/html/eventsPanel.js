b3b94ef61a63c9d497c78ff4bea75676fc468cc7
Simon Lindholm <simon.lindholm10@gmail.com>
2013-09-11 02:17:31 +0200

    derived listeners

--- extension/content/firebug/html/eventsPanel.js
+++ extension/content/firebug/html/eventsPanel.js
@@ -1,31 +1,31 @@
 /* See license.txt for terms of usage */
-/*jshint esnext:true, curly:false*/
-/*global FBTrace:true, Document:true, Window:true, define:true */
+/*jshint esnext:true, curly:false, loopfunc:true*/
+/*global FBTrace:1, Components:1, define:1 */
 
 // TODO:
 // UI:
-// - derived listeners
+// - derived listeners (simple UI!)
 // - clicking elements in headers, probably (though it's problematic with `window` which covers the whole line)
 //  - or else right-clicking elements in headers
 // - clicking event handlers doesn't do anything (are they even in the script panel?)
 // - collapsed headers shouldn't have spacing between them
 // - styling of event groups, collapsible?, headery
+// - replace right arrow symbol by image, for stability
 // - capture
 // - a11y
 // Functionality:
-// - derived listeners:
-//   * closures
-//   * jQuery special case
 // - detect eventbug
 // - applicationCache etc.
+// - mutation observers
 // Other:
 // - new issue about having source code as title
 // - seeing closures of event listeners??
+// - dynamic updates for jQuery listeners will be awful. watch('length') technically works.
 
 // Testing TODO:
 // - disabling event listener, event handlers, attribute event handlers
 // - duplicate listeners and such
-// - jQuery
+// - derived listeners (jQuery and others)
 // - capture, source links
 
 define([
@@ -37,14 +37,16 @@ define([
     "firebug/lib/locale",
     "firebug/lib/events",
     "firebug/chrome/reps",
+    "firebug/debugger/debuggerLib",
 ],
-function(Obj, Firebug, Domplate, Dom, Wrapper, Locale, Events, FirebugReps) {
+function(Obj, Firebug, Domplate, Dom, Wrapper, Locale, Events, FirebugReps, DebuggerLib) {
 "use strict";
 
 // ********************************************************************************************* //
 // Constants
 
 var {domplate, DIV, FOR, TAG, H1, H2, SPAN} = Domplate;
+var Cu = Components.utils;
 
 // ********************************************************************************************* //
 // Events Panel (HTML side panel)
@@ -98,12 +100,19 @@ EventsPanel.prototype = Obj.extend(Firebug.Panel,
             ),
 
         listenerTag:
-            DIV({"class": "listenerLine", $disabled: "$listener.disabled",
+            DIV({"class": "listenerLineGroup", $disabled: "$listener.disabled",
                 _listenerObject: "$listener"},
-                SPAN({"class": "listenerIndent", "role": "presentation"}),
-                TAG(FirebugReps.Func.tag, {object: "$listener.func"}),
-                SPAN({"class": "listenerCapturing", "hidden": "$listener|notCapturing"}, "C"), // XXX
-                TAG(FirebugReps.SourceLink.tag, {object: "$listener.sourceLink"})
+                DIV({"class": "listenerLine originalListener"},
+                    SPAN({"class": "listenerIndent", "role": "presentation"}),
+                    TAG(FirebugReps.Func.tag, {object: "$listener.func"}),
+                    SPAN({"class": "listenerCapturing", "hidden": "$listener|notCapturing"}, "C"), // XXX
+                    TAG(FirebugReps.SourceLink.tag, {object: "$listener.sourceLink"})),
+                FOR("derivedListener", "$listener.derivedListeners",
+                    DIV({"class": "listenerLine derivedListener"},
+                        SPAN({"class": "listenerIndent", "role": "presentation"}),
+                        TAG(FirebugReps.Func.tag, {object: "$derivedListener.func"}),
+                        TAG(FirebugReps.SourceLink.tag, {object: "$derivedListener.sourceLink"}))
+                )
             ),
 
         noOwnListenersTag:
@@ -172,16 +181,96 @@ EventsPanel.prototype = Obj.extend(Firebug.Panel,
         return context.listenerDisabledMap;
     },
 
+    getDerivedListeners: function(func, type, target)
+    {
+        // Try to see if the listener (often from a library) wraps another user-defined listener,
+        // and if so extract the user-defined listener(s). We do this through pattern-matching on
+        // function calls that go through call or apply, which are often used to set 'this' to
+        // something which is reasonable from a library user's point of view, but are rather
+        // uncommon outside of library code. We then use debugger magic to extract the original
+        // functions from the listener's closure.
+        var src = func + "";
+        var mIndirection = /\b([a-zA-Z_$][a-zA-Z0-9_$]*)\.(call|apply)/.exec(src);
+        if (!mIndirection)
+            return null;
+        var funcName = mIndirection[1];
+
+        var global = Cu.getGlobalForObject(func);
+        var dglobal = DebuggerLib.getDebuggeeGlobal(this.context, global);
+        var dfunc = dglobal.makeDebuggeeValue(func);
+        var env = dfunc && dfunc.environment;
+
+        if (src.charAt(mIndirection.index - 1) === ".")
+        {
+            // Not a direct call; bail. Before we give up entirely, try one last special case:
+            // jQuery. For reasons of old-IE compat and extensibility, jQuery (and only jQuery)
+            // stores all event listeners in a data structure separated from the closure of the
+            // listener function. We special-case it only because it is so common.
+            return this.getDerivedJqueryListeners(target, type, env, funcName, src);
+        }
+
+        env = env && env.find(funcName);
+        if (!env || !env.parent)
+            return null;
+        var dderivedF = env.getVariable(funcName);
+        var derivedF = DebuggerLib.unwrapDebuggeeValue(dderivedF);
+        if (typeof derivedF !== "function")
+            return null;
+        return [derivedF];
+    },
+
+    getDerivedJqueryListeners: function(target, type, env, funcName, src)
+    {
+        if (!(funcName === "handle" || funcName === "dispatch") || !env)
+            return null;
+        try
+        {
+            // Pattern match on the occurance of '<minified name>.event.<handle or dispatch>.apply'.
+            var matches = /\b([a-zA-Z_$][a-zA-Z0-9_$]*)\.event\.(dispatch|handle)\.apply/.exec(src);
+            var jqName = (matches && matches[1]) || "";
+            var env2 = env.find(jqName);
+            var djq = env2 && env2.getVariable(jqName);
+            if (!djq)
+                return null;
+
+            var jq = DebuggerLib.unwrapDebuggeeValue(djq);
+            var fnName = ("_data" in jq ? "_data" : "data");
+            var eventData = jq[fnName](target, "events");
+            var listeners = eventData && eventData[type];
+            if (!listeners)
+                return null;
+
+            var ret = [];
+            for (var i = 0; i < listeners.length; i++)
+                ret.push(listeners[i].handler);
+            return ret;
+        }
+        catch (exc)
+        {
+            return null;
+        }
+    },
+
     getNormalEventListeners: function(target)
     {
         var context = this.context;
         var listeners = Events.getEventListenersForTarget(target);
         var hasOneHandler = new Set();
+        var self = this;
         listeners.forEach(function(li)
         {
             li.disabled = false;
             li.target = target;
             li.sourceLink = Firebug.SourceFile.findSourceForFunction(li.func, context);
+
+            var derived = self.getDerivedListeners(li.func, li.type, target) || [];
+            li.derivedListeners = derived.map(function(listener)
+            {
+                return {
+                    func: listener,
+                    sourceLink: Firebug.SourceFile.findSourceForFunction(listener, context)
+                };
+            });
 
             var handlerName = "on" + li.type;
             if (handlerName in Object.getPrototypeOf(target) &&
@@ -198,7 +287,7 @@ EventsPanel.prototype = Obj.extend(Firebug.Panel,
                 li.disable = function()
                 {
                     target[handlerName] = null;
-                }
+                };
             }
             else
             {
@@ -359,9 +448,10 @@ EventsPanel.prototype = Obj.extend(Firebug.Panel,
             this.toggleGroup(header.parentNode);
             Events.cancelEvent(event);
         }
-        else if (target.classList.contains("listenerIndent"))
+        else if (target.classList.contains("listenerIndent") &&
+            target.parentNode.classList.contains("originalListener"))
         {
-            var row = Dom.getAncestorByClass(target, "listenerLine");
+            var row = Dom.getAncestorByClass(target, "listenerLineGroup");
             this.toggleDisableRow(row);
             Events.cancelEvent(event);
         }
