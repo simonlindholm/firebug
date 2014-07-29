643ae3b6e187bf6ed1bc94432fdda00afebd80dc
Simon Lindholm <simon.lindholm10@gmail.com>
2014-07-30 01:34:24 +0200

    Issue 7597: Console API is broken since Firefox 33
    https://code.google.com/p/fbug/issues/detail?id=7597

--- extension/content/firebug/console/consoleInjector.js
+++ extension/content/firebug/console/consoleInjector.js
@@ -38,35 +38,22 @@ Firebug.Console.injector =
             // Get the 'console' object (this comes from chrome scope).
             var console = ConsoleExposed.createFirebugConsole(context, win);
 
-            // Do not expose the chrome object as is but, rather do a wrapper, see below.
-            //win.wrappedJSObject.console = console;
-            //return;
-
-            // Construct a script string that defines a function. This function returns
-            // an object that wraps every 'console' method. This function will be evaluated
-            // in a window content sandbox and return a wrapper for the 'console' object.
-            // Note that this wrapper appends an additional frame that shouldn't be displayed
-            // to the user.
-            //
-            // Since we are using .caller and .arguments for stack walking, the function must
-            // not be in strict mode.
-            var expr = "(function(x) { return {\n";
-            for (var p in console)
+            // Create a content-owned "console" object, to be exported into the page.
+            var exposedConsole = new win.Object();
+            exposedConsole = XPCNativeWrapper.unwrap(exposedConsole);
+            for (var prop in console)
             {
-                var func = console[p];
-                if (typeof(func) == "function")
-                {
-                    expr += p + ": function() { return Function.apply.call(x." + p +
-                        ", x, arguments); },\n";
-                }
+                if (typeof console[prop] !== "function")
+                    continue;
+                var desc = {
+                    writable: true,
+                    configurable: true,
+                    enumerable: true,
+                    value: console[prop]
+                };
+                Object.defineProperty(exposedConsole, prop, desc);
             }
-            expr += "};})";
-
-            // Evaluate the function in the window sandbox/scope and execute. The return value
-            // is a wrapper for the 'console' object.
-            var sandbox = Cu.Sandbox(win);
-            var getConsoleWrapper = Cu.evalInSandbox(expr, sandbox);
-            var exposedConsole = getConsoleWrapper(console);
+            Cu.makeObjectPropsNormal(exposedConsole);
 
             // Store the context and the exposedConsole in a WeakMap.
             wmExposedConsoles.set(winDoc, {
